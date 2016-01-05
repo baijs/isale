ddf0a31c75c6e56160ca6bc5dac7380ddc6a8f53
baijs <flyjs.bai@foxmail.com>
2016-01-05 16:34:22 +0800

    update

--- app/resources/javascripts/app.js
+++ app/resources/javascripts/app.js
@@ -4,47 +4,49 @@
  * angular js main file
  */
 
-var module = angular.module('isaleApp',['ngRoute']);
+var module = angular.module('isaleApp', ['ngRoute']);
 
-module.config(function($routeProvider){
+module.config(function ($routeProvider) {
     $routeProvider
-        .when('/order',{
-            controller:'OrderController',
-            templateUrl:'/isale/components/order.html'
+        .when('/order', {
+            controller: 'OrderController',
+            templateUrl: '/isale/components/order.html'
         })
         .otherwise({
             redirectTo: '/'
         });
 });
 
-module.directive('headBar',function(){
+module.directive('headBar', function () {
     return {
-        restrict:'EA',
-        templateUrl:'/isale/tpl/head.html',
-        replace:true
+        restrict: 'EA',
+        templateUrl: '/isale/tpl/head.html',
+        replace: true
     }
 });
 
-module.directive('menuBar',function(){
+module.directive('menuBar', function () {
     return {
-        restrict:'EA',
-        templateUrl:'/isale/tpl/menu.html',
-        replace:true
+        restrict: 'EA',
+        templateUrl: '/isale/tpl/menu.html',
+        replace: true
     }
 });
-module.directive('footBar',function(){
+module.directive('footBar', function () {
     return {
-        restrict:'EA',
-        templateUrl:'/isale/tpl/foot.html',
-        replace:true
+        restrict: 'EA',
+        templateUrl: '/isale/tpl/foot.html',
+        replace: true
     }
 });
 
 
-
-module.controller('OrderController',function($scope,$http){
-    $http.get("/isale/testdata")
-        .success(function (response) {$scope.names = response.records;});
+module.controller('OrderController', function ($scope, $http) {
+    $http.get("/isale/order")
+        .success(function (response) {
+            $scope.records = response.records;
+            $scope.total = response.total;
+        });
 });
 
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,25 +6,16 @@ router.get('/', function (req, res, next) {
     res.render('index', {title: 'Express'});
 });
 
-router.get('/testdata', function (req, res, next) {
+router.get('/order', function (req, res, next) {
     var data = {
         "records": [
-            {"Name": "北京市", "City": "Berlin", "Country": "Germany"},
-            {"Name": "Ana Trujillo Emparedados y helados", "City": "México D.F.", "Country": "Mexico"},
-            {"Name": "Antonio Moreno Taqueria", "City": "México D.F.", "Country": "Mexico"},
-            {"Name": "Around the Horn", "City": "London", "Country": "UK"},
-            {"Name": "B's Beverages", "City": "London", "Country": "UK"},
-            {"Name": "Berglunds snabbkap", "City": "Luleå", "Country": "Sweden"},
-            {"Name": "Blauer See Delikatessen", "City": "Mannheim", "Country": "Germany"},
-            {"Name": "Blondel pre et fils", "City": "Strasbourg", "Country": "France"},
-            {"Name": "Bólido Comidas preparadas", "City": "Madrid", "Country": "Spain"},
-            {"Name": "Bon app'", "City": "Marseille", "Country": "France"},
-            {"Name": "Bottom-Dollar Marketse", "City": "Tsawassen", "Country": "Canada"},
-            {"Name": "Cactus Comidas para llevar", "City": "Buenos Aires", "Country": "Argentina"},
-            {"Name": "Centro comercial Moctezuma", "City": "México D.F.", "Country": "Mexico"},
-            {"Name": "Chop-suey Chinese", "City": "Bern", "Country": "Switzerland"},
-            {"Name": "Comrcio Mineiro", "City": "São Paulo", "Country": "Brazil"}
-        ]
+            {"id": "01","name":"商品","orderId":"11111","num":"10","price":"190.00","account":"b@163.com","remark":"无","paymethod":"支付宝" },
+            {"id": "01","name":"商品","orderId":"11111","num":"10","price":"190.00","account":"b@163.com","remark":"无","paymethod":"支付宝" },
+            {"id": "01","name":"商品","orderId":"11111","num":"10","price":"190.00","account":"b@163.com","remark":"无","paymethod":"支付宝" },
+            {"id": "01","name":"商品","orderId":"11111","num":"10","price":"190.00","account":"b@163.com","remark":"无","paymethod":"支付宝" },
+            {"id": "01","name":"商品","orderId":"11111","num":"10","price":"190.00","account":"b@163.com","remark":"无","paymethod":"支付宝" }
+        ],
+        "total":100
     };
     res.send(data);
 });
